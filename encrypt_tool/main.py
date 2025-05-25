4e7b0218f99207799bc655613cee3854d7f29e49
d3giigii <lhammond997@gmail.com>
2025-05-25 01:38:03 -0400

    Include custom file names, docstrings, and improved error handling. Update .gitignore.

--- encrypt_tool/main.py
+++ encrypt_tool/main.py
@@ -1,16 +1,28 @@
+"""
+Encrypt-Tool CLI
+
+This module provides a simple command-line interface for secure file encryption
+and decryption using AES-128 in OCB mode. Users can create text files, encrypt
+them with authenticated encryption, decrypt them with integrity verification,
+and clean up working files.
+
+All operations occur in a 'working/' directiory for isolated file handling.
+
+Author: Logan Hammond; lhammond997@gmail.com
+"""
+
 import os
 
 from Cryptodome.Cipher import AES
 from Cryptodome.Random import get_random_bytes
 
-# TODO: Create and read files from a working directory rather than the parent. 
-# TODO: Verify proper error handling. 
-# TODO: Verify docstrings are correct.
-# TODO: Make linter happy. 
+def display_menu() -> None:
+    """Display the menu to user in the CLI.
 
-def display_menu():
-    """Display the menu to user in the CLI."""
-    
+    Returns:
+        None
+    """
+
     display_menu.options = [
         "Create a file.",
         "Encrypt a file.",
@@ -18,206 +30,263 @@ def display_menu():
         "Cleanup files.",
         "Exit."
     ]
-    
+
     print()
     for i, txt in enumerate(display_menu.options):
         print(f"{i+1}: {txt}")
 
-def is_valid_input():
-    """Return True if input is valid for program schema."""
-    # TODO: Add schema and logic validation for input. 
+def sanitize_input(txt: str) -> str:
+    """Return a copy of the string with suspect characters removed.
 
-    # Schema validation. 
-
-    # Logic validation. 
-
-def sanitize_input(txt: str):
-    """Return a copy of the string with suspect characters removed."""
-    INVALID_CHARS = [';', '|', '&', "\"", "\'"]
-    return "".join([i for i in str(txt) if not i in INVALID_CHARS])
-
-def get_input(txt):
-    """Return a copy of input from the user after it is sanitized."""
-    raw = int
-    try:
-        # Get input from user. 
-        raw = input(f"{txt}").strip()
-        raw = sanitize_input(raw)
-    except Exception:
-        print("Error: Invalid input. Try again.")
-
-    # TODO: Linter is making raw red. Should be yellow if anything. 
-    return raw
-
-def get_menu_input() -> int:
-    """Retrieve choice from user in the CLI.
-
-    Raises:
-        Exception: User input is not exactly a single integer. 
+    Args:
+        txt (str): The input string to sanitize.
 
     Returns:
-        int: Sanitized user input. 
+        str: A cleaned version of the input string.
     """
 
-    """
-    Thoughts: While raised exceptions should be as narrow as possible, for the 
-    purposes of maximizing security and reliability (which I am trying to 
-    showcase), a broad catch-all exception works well. 
+    invalid_chars = [';', '|', '&', "\"", "\'"]
+    return "".join([i for i in str(txt) if not i in invalid_chars])
+
+def get_input(txt: str) -> str:
+    """Return a copy of input from the user after it is sanitized.
+
+    Args:
+        txt (str): Prompt to display to the user.
+
+    Returns:
+        str: Sanitized user input string.
     """
 
-    raw = None
+    raw = input(f"{txt}").strip()
+    return sanitize_input(raw)
+
+def get_menu_input() -> int:
+    """Retrieve choice from user in the CLI menu.
+
+    Raises:
+        Exception: User input is not exactly a single integer.
+
+    Returns:
+        int: Sanitized user input.
+    """
+
     try:
-        # Get input from user. 
-        raw = int(input("Select option: ").strip())
-        raw = int(sanitize_input(raw))
-
-        # Schema validation. Input should be a single integer. 
+        raw = int(get_input("Select option: "))
+        # Schema validation. Input should be a single integer.
         if not isinstance(raw, int) or len(str(raw)) != 1:
-            raise Exception
-        
+            raise ValueError
         # Logic validation. Input should be between 1 and the number of options.
         if not 0 <= raw <= len(display_menu.options):
-            raise Exception
-    
-    except Exception:
-        print("Error: Invalid input. Try again.")
+            raise ValueError
+        return raw
+    except ValueError as e:
+        print(f"Error: {e}")
 
-    return raw
+    return int()
 
-def create_file():
+def create_file(file_name: str, file_msg: str) -> None:
+    """Create a file with given name and message.
+
+    Args:
+        file_name (str): Name of the file to create.
+        file_msg (str): Message content to write into the file.
+
+    Returns:
+        None
+    """
+
     print("Create file selected.")
 
-    # Get file name and message from user. 
-    file_name = get_input("Enter file name: ")
-    message = get_input("Enter message: ")
-    if not ".txt" in file_name:
+    # Ensure file name ends with .txt.
+    if not (len(file_name) >= 4 and file_name[-4:] == ".txt"):
         file_name += ".txt"
 
-    # Create file with cleartext message. 
-    with open(f"{file_name}", "w+") as file:
-        file.writelines(message)
+    # Create file with cleartext message.
+    with open(f"working/{file_name}", "w+", encoding="utf-8") as file:
+        file.writelines(file_msg)
 
-def encrypt_file():
-    # TODO: Catch FileNotFoundError when cleartext has an extension but user 
-    # enters a file name with none. 
+def encrypt_file(file_name: str) -> None:
+    """Encrypt a given file in the working directory.
+
+    Args:
+        file_name (str): Name of the plaintext file (without extension) to
+        encrypt.
+
+    Returns:
+        None
+
+    Raises:
+        FileNotFoundError: If the specified file does not exist.
+    """
 
     print("Encrypt file selected.")
 
-    file_name = get_input("Enter file name: ")
-    file_data = None
+    # Read in cleartext message.
+    try:
+        with open(f"working/{file_name}.txt", "r", encoding="utf-8") as file:
+            file_data = file.read()
+    except FileNotFoundError:
+        print(f"Error: Can not find file {file_name}.")
+        return
 
-    # Read in cleartext message. 
-    with open(f"{file_name}", "r") as file:
-        file_data = file.read()
-
-    # Covert cleartext to bytes. 
+    # Covert cleartext to bytes.
     data = file_data.encode()
 
-    # Generate 128 bit (16 byte) AES key and save it for later use.  
-    # TODO: Add encryption for the key as well. 
+    # Generate 128 bit (16 byte) AES key and save it for later use.
+    # TODO: Add encryption for the key as well.
     aes_key = get_random_bytes(16)
-    with open("aes.key", "wb") as file:
+    with open(f"working/{file_name}_aes.key", "wb") as file:
         file.write(aes_key)
 
-    # Create AES cipher. OCB mode provides confidentiality and integrity. 
+    # Create AES cipher. OCB mode provides confidentiality and integrity.
     cipher = AES.new(aes_key, AES.MODE_OCB)
 
-    # Encrypt message and authenticate. Cipher tag verifies integrity. 
+    # Encrypt message and authenticate. Cipher tag verifies integrity.
     cipher_text, cipher_tag = cipher.encrypt_and_digest(data)
     assert len(cipher.nonce) == 15
 
-    # Save encrypted message. 
-    with open("message_encrypted.bin", "wb") as file:
+    # Save encrypted message.
+    with open(f"working/{file_name}_encrypted.bin", "wb") as file:
         file.write(cipher_tag)
         file.write(cipher.nonce)
         file.write(cipher_text)
 
-def decrypt_file():
+def decrypt_file(file_name: str, key_name: str) -> None:
+    """Decrypt an encrypted file in the working directory.
+
+    Returns:
+        None
+
+    Raises:
+        FileNotFoundError: If the AES key or message file does not exist.
+        ValueError: If decryption fails due to tampering or invalid
+        authentication.
+        AssertionError: If required components are not properly loaded or typed.
+    """
+
     print("Decrypt file selected.")
 
-    # Load the AES key. 
-    try: 
-        with open("aes.key", "rb") as key_file:
+    # Load the AES key.
+    try:
+        with open(f"working/{key_name}", "rb") as key_file:
             aes_key = key_file.read()
     except FileNotFoundError:
-        print("Error: No key file found. Regenerate message.")        
-    except Exception as e:
-        print(f"Error: {e}")
+        print("Error: No key file found. Regenerate message.")
+        return
 
-    # Load the encrypted file. 
+    # Load the encrypted file.
     try:
         cipher_tag, cipher_nonce, cipher_text = None, None, None
-        with open("message_encrypted.bin", "rb") as file:
+        with open(f"working/{file_name}", "rb") as file:
             cipher_tag = file.read(16)
             cipher_nonce = file.read(15)
             cipher_text = file.read()
     except FileNotFoundError:
         print("Error: Message file not found. Regenerate message.")
-    except Exception as e:
+        return
+
+    # Decrypt the file.
+    try:
+        assert all([aes_key, cipher_tag, cipher_nonce, cipher_text])
+    except AssertionError as e:
         print(f"Error: {e}")
-    
-    # Decrypt the file. 
+
     should_save_file = False
     cipher = AES.new(aes_key, AES.MODE_OCB, nonce=cipher_nonce)
     try:
+        assert isinstance(cipher_text, bytes)
+        assert isinstance(cipher_tag, bytes)
         clear_text = cipher.decrypt_and_verify(cipher_text, cipher_tag)
         print(f"Message: {clear_text.decode()}")
         should_save_file = True
+    except AssertionError as e:
+        print(f"Error: {e}")
     except ValueError:
-        print("Warning: The file was altered! Decrypted message not saved.")
-     
-    # Save decrypted message to file. 
+        print("Warning: The file was altered! Decrypted message will not be " \
+        "saved.")
+
+    # Save decrypted message to file.
     if should_save_file:
         try:
-            with open("message_decrypted.txt", "wb") as file:
+            with open(f"working/{file_name}_decrypted.txt", "wb") as file:
                 file.write(clear_text)
-        except Exception as e:
+        except PermissionError as e:
             print(f"Error: {e}")
 
-def cleanup_files():
-    
-    files_to_remove = [
-        "aes.key",
-        "message.txt",
-        "message_encrypted.bin",
-        "message_decrypted.txt"
-    ]
+def cleanup_files() -> None:
+    """Remove any files in working directory.
 
-    for file in files_to_remove:        
-        try: 
-            os.remove(file)
-        except FileNotFoundError as e:
-            print(f"Warning: {e}")
-        except Exception as e:
+    Returns:
+        None
+
+    Raises:
+        OSError: If file removal encounters an error.
+    """
+
+    for file in os.listdir("working"):
+        try:
+            os.remove(f"working/{file}")
+        except OSError as e:
             print(f"Error: {e}")
 
 def main():
-    # Welcome user. 
-    print("\nWelcome to Encrypt-Tool!")    
+    """Prompt user and process input.
+
+    Returns:
+        None
+
+    Raises:
+        OSError: If working directory cannot be created.
+    """
+
+    # Create working directory if it does not alread exist.
+    try:
+        if not os.path.exists("working/"):
+            os.makedirs("working")
+    except OSError as e:
+        print(f"Error: {e}")
+
+    # Welcome user.
+    print("\nWelcome to Encrypt-Tool!")
 
     # Enter main program loop.
     while True:
-        
-        # Display menu to the user. 
+
+        # Display menu to the user.
         display_menu()
 
-        # Get the menu choice from user. 
+        # Get the menu choice from user.
         user_input = get_menu_input()
 
-        # Process the menu choice from user. 
+        # Process the menu choice from user.
         if user_input == 1:
-            create_file()
+            file_name = get_input("Enter file name: ")
+            file_msg = get_input("Enter message: ")
+            create_file(file_name, file_msg)
         elif user_input == 2:
-            encrypt_file()
+            file_name = get_input("Enter file name: ")
+            encrypt_file(file_name)
         elif user_input == 3:
-            decrypt_file()
+            file_name = get_input("Enter file name: ")
+            key_name = get_input("Enter key name: ")
+            decrypt_file(file_name, key_name)
         elif user_input == 4:
             cleanup_files()
         elif user_input == 5:
             break
 
-    # Thank user. 
+    # Thank user.
     print("\nThank you for using Encrypt-Tool!\n")
 
 def cli():
+    """Hook to main for built project.
+
+    Returns:
+        None
+    """
+
+    main()
+
+if __name__ == "__main__":
     main()
